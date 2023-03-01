b53df9302a6e2ddb6cc471c0336e77e141e462e1
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-01 10:21:48 +0000

    .

---
+++ api/spec/controllers/routes.test.js
@@ -0,0 +1,13 @@
+const app = require('../../app');
+const request = require('supertest');
+require('../mongodb_helper');
+const Route = require('../../models/routes');
+const Location = require('../../models/location');
+
+describe('/routes', () => {
+  beforeAll(() => {
+    //creates routes and locations
+  });
+
+  describe('GET, when email and password are provided', () => {});
+});
