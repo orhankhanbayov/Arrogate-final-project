fda395fc31ac35d1eb5aa97cc785f9aa0d2b7927
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-02 16:09:24 +0000

    /

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {  ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import LogInForm from './src/components/auth/LoginForm';
 import SignUpForm from './src/components/user/SignUpForm';
+import MainContainer from './src/components/navigation/mainContainer';
+
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -12,13 +14,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-    <Stack.Screen name="LogIn" component={LogInForm} />
-    <Stack.Screen name="SignUp" component={SignUpForm} />
-        
-     
-      
-    
-     </Stack.Navigator>
+        <Stack.Screen name="LogIn" component={LogInForm} />
+        <Stack.Screen name="SignUp" component={SignUpForm} />
+        <Stack.Screen name="mainContainer" component={MainContainer} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
