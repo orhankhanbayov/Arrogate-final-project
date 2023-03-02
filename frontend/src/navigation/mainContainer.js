e9bd8d7e1a26ccf848f3bd0d7aebc1c34483d547
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-02 14:21:42 +0000

    navbar

---
+++ frontend/src/navigation/mainContainer.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import HomeScreen from './screens/homeScreen';
+import SettingsScreen from './screens/settingsScreen';
+
+const homeName = 'Home';
+const settingsName = 'Settings';
+
+const Tab = createBottomTabNavigator();
+
+const MainContainer = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (rn === settingsName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainContainer;
---
+++ frontend/src/navigation/screens/settingsScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const SettingsScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+};
+
+export default SettingsScreen;
