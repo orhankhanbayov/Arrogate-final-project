d81c396fc86a951b8721b776ae70dd751e821261
David Scott <sirdavidscott@hotmail.com>
2023-03-03 16:30:05 +0000

    checking user geolocation against fetch request of location in db specifying The Shard

--- frontend/gpsLocation.js
+++ frontend/gpsLocation.js
@@ -6,48 +6,83 @@ import { getDistance, getPreciseDistance  } from 'geolib';
 
 export default function GPSLocation() {
   const [location, setLocation] = useState();
+  const [locationLat, setLocationLat] = useState();
+  const [locationLong, setLocationLong] = useState();
   
   useEffect(() => {
-    const getPermissions = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            console.log("Please grant location permissions");
-            return;
+
+    // const token = AsyncStorage.getItem("token")
+
+    const getLocationCoords = async () => {
+      try {
+        const response = await fetch(
+          'https://mystery-route-backend.onrender.com/routes',
+          {
+            method: 'get',
+            headers: {
+              //  Authorization: `Bearer ${token}`,
+              "Content-Type": 'application/json',
+            },
           }
-          
-        let currentLocation = await Location.getCurrentPositionAsync({});
-        setLocation(currentLocation);
-        console.log("Location:");
-        console.log(currentLocation);
-        console.log(currentLocation.coords.latitude);
-        console.log(currentLocation.coords.longitude);
-        
-        lat = currentLocation.coords.latitude;
-        long = currentLocation.coords.longitude;
-        
-        console.log(lat, long)
-        
-        dis1 = getPreciseDistance(
-            {latitude:lat, longitude:long}, // user's coords
-            {latitude:51.519522, longitude: -0.033101}, // target location coord
         );
-        console.log(dis1);
-
-        if (dis1 > 50) {
-          console.log("too far away!")
-        } else {
-          console.log("Congratulations, you found it!")
+    
+        if (!response.ok) {
+          throw new Error(response.statusText);
         }
+    
+        const data = await response.json();
 
+        // console.log(data.routes[0].locations[0].coordinates.coordinates) // the zeroes in here must be changed to reference the correct route number and location number
+        const locationLat = data.routes[0].locations[0].coordinates.coordinates[0]
+        const locationLong = data.routes[0].locations[0].coordinates.coordinates[1]
+        // console.log(locationLat)
+        // console.log(locationLong)
+
+        setLocationLat(locationLat);
+        setLocationLong(locationLong);
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getLocationCoords()
+
+
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Please grant location permissions");
+        return; 
+      }
+        
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      // console.log("Location:");
+      // console.log(currentLocation);
+      // console.log(currentLocation.coords.latitude);
+      // console.log(currentLocation.coords.longitude);
+      
+      lat = currentLocation.coords.latitude;
+      long = currentLocation.coords.longitude;
+      
+      // console.log(lat, long)
+      
+      dis1 = getPreciseDistance(
+        {latitude:lat, longitude:long}, // user's coords
+        {latitude:locationLat, longitude:locationLong}, // target location coord
+      );
+      // console.log(dis1);
+
+      if (dis1 > 100) {
+        console.log("Wrong place")
+      } else if (dis1 > 50 && dis1 < 100) {
+        console.log("Getting close!")
+      } else {
+        console.log("Congratulations, you found it!")
+      }
     };
     getPermissions();
   
   }, []);
-
-// if latitude and longitude = National Theatre latitude and longitude
-// congratulations page
-// if not equal returns to clues page (with bad luck message)
-
-
-
 }
+
