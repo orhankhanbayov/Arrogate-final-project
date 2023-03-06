63ba7f6a43242e3b12a723116aecaf2150eddfb2
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-06 17:32:53 +0000

    /

--- frontend/src/components/navigation/screens/mapScreen.js
+++ frontend/src/components/navigation/screens/mapScreen.js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import * as SecureStore from 'expo-secure-store';
 import { NavigationContainer } from '@react-navigation/native';
-
+import * as Location from 'expo-location';
 import {
   Image,
   StyleSheet,
@@ -15,18 +15,31 @@ import {
   TouchableOpacity,
 } from 'react-native';
 const MapScreen = ({ navigation }) => {
+  const [location, setLocation] = useState();
+
+  const getUserCoords = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      return;
+    }
+
+    let currentLocation = await Location.getCurrentPositionAsync({});
+    setLocation(currentLocation);
+  };
+  getUserCoords();
   // useEffect(() => {});
   return (
     <MapView
       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
       style={styles.map}
       region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
+        latitude: 51.506554,
+        longitude: -0.129115,
+        latitudeDelta: 0.000808,
+        longitudeDelta: 0.000204,
       }}
       showsMyLocationButton={true}
+      showsUserLocation={true}
     ></MapView>
   );
 };
