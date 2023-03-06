634b636dd019e62328a6d07178dd7bbf903577db
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-06 22:36:52 +0000

    /

--- api/controllers/routes.js
+++ api/controllers/routes.js
@@ -29,6 +29,20 @@ const RoutesController = {
       }
     });
   },
+
+  Update: (req, res) => {
+    Route.findById(req.body.id, async (err, route) => {
+      if (err) {
+        throw err;
+      } else {
+        const token = await TokenGenerator.jsonwebtoken(req.user_id);
+
+        route.start = req.body.start;
+        await route.save();
+        res.status(204).json({ message: 'OK', token: token });
+      }
+    });
+  },
 };
 
 module.exports = RoutesController;
