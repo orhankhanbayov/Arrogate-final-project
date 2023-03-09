46217e24b7fe217995bfcb4fa8a502f613c688e8
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-09 05:13:56 +0000

    /

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -3,21 +3,28 @@ const TokenGenerator = require('../models/token_generator');
 
 const AccountController = {
   updateScore: (req, res) => {
-    User.findOne(
-      { email: req.body.email },
-      { $inc: { trophies: req.body.trophies, coins: req.body.coins } },
-      async (err, user) => {
-        if (err) {
-          throw err;
+    User.findById(req.body.id, async (err, user) => {
+      if (err) {
+        throw err;
+      } else {
+        const token = await TokenGenerator.jsonwebtoken(req.user_id);
+
+        // Check if trophies and coins properties exist
+        if (user.hasOwnProperty('trophies') && user.hasOwnProperty('coins')) {
+          // Increment trophies and coins
+          user.trophies = parseInt(user.trophies) + parseInt(req.body.trophies);
+          user.coins = parseInt(user.coins) + parseInt(req.body.coins);
         } else {
-          const token = await TokenGenerator.jsonwebtoken(req.user_id);
-
-          res.status(204).json({ message: 'OK', token: token });
+          // Set default values and increment
+          user.trophies = parseInt(req.body.trophies || 0);
+          user.coins = parseInt(req.body.coins || 0);
         }
-      }
-    );
-  },
 
+        await user.save();
+        res.status(204).json({ message: 'OK', token: token });
+      }
+    });
+  },
   getScore: (req, res) => {
     User.findById(req.body.id, { trophies: 1, coins: 1 }, async (err, user) => {
       if (err) {
